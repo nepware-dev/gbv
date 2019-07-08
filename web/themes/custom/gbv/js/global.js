a70dd835644edb413b66d4a69f20b1ef9f86885d
Bishal Timilsina <bishaltimilsina@gmail.com>
2019-07-09 00:07:08 +0545

    Gbv fixes part-1

--- web/themes/custom/gbv/js/global.js
+++ web/themes/custom/gbv/js/global.js
@@ -5,30 +5,30 @@
  */
 (function($, Drupal) {
 
-  'use strict';
+    'use strict';
 
-  Drupal.behaviors.gbv = {
-    attach: function(context, settings) {
-      var position = $(window).scrollTop();
-      $(window).scroll(function () {
-        if ($(this).scrollTop() > 50) {
-          $('body').addClass("scrolled");
-        }
-        else {
-          $('body').removeClass("scrolled");
-        }
-        var scroll = $(window).scrollTop();
-        if (scroll > position) {
-          $('body').addClass("scrolldown");
-          $('body').removeClass("scrollup");
-        } else {
-          $('body').addClass("scrollup");
-          $('body').removeClass("scrolldown");
-        }
-        position = scroll;
-      });
+    Drupal.behaviors.gbv = {
+        attach: function(context, settings) {
+            let position = $(window).scrollTop();
+            $(window).scroll(function () {
+                if ($(this).scrollTop() > 50) {
+                    $('body').addClass('scrolled');
+                }
+                else {
+                    $('body').removeClass('scrolled');
+                }
+                let scroll = $(window).scrollTop();
+                if (scroll > position) {
+                    $('body').addClass('scrolldown');
+                    $('body').removeClass('scrollup');
+                } else {
+                    $('body').addClass('scrollup');
+                    $('body').removeClass('scrolldown');
+                }
+                position = scroll;
+            });
 
-    }
-  };
+        }
+    };
 
 })(jQuery, Drupal);
