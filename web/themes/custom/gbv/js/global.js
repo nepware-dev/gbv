89cff6f88105b8b50d24368fe2837216043191db
saugatdhimal <dhimalsaugat77@gmail.com>
2021-09-27 16:56:37 +0545

    Added key figures counter up

--- web/themes/custom/gbv/js/global.js
+++ web/themes/custom/gbv/js/global.js
@@ -59,4 +59,102 @@
         });
     }
 
+    let numbers = [];
+    let countryNumbers = [];
+
+  for (
+    let i = 0;
+    i < $(".key-figure-wrapper .view-content").children().length;
+    i++
+  ) {
+    numbers.push(
+      $(`.key-figure-wrapper .view-content .views-row:nth-child(${i + 1}) p`)
+        .html()
+        .match(/\d+/)
+    );
+  }
+
+  for (
+    let i = 0;
+    i < $(".view-country-key-figures .view-content").children().length;
+    i++
+  ) {
+    countryNumbers.push(
+      $(`.view-country-key-figures .view-content .views-row:nth-child(${i + 1}) p`)
+        .html()
+        .match(/\d+/)
+    );
+  }
+
+  for (
+    let i = 0;
+    i < $(".key-figure-wrapper .view-content").children().length;
+    i++
+  ) {
+    let str = $(
+      `.key-figure-wrapper .view-content .views-row:nth-child(${i + 1}) p`
+    ).html();
+    let newStr = str.replace(
+      numbers[i],
+      `<span class='counter' data-count=${numbers[i]}>${numbers[i]}</span>`
+    );
+    $(
+      `.key-figure-wrapper .view-content .views-row:nth-child(${i + 1}) p`
+    ).html(newStr);
+  }
+
+  for (
+    let i = 0;
+    i < $(".view-country-key-figures .view-content").children().length;
+    i++
+  ) {
+    let str = $(
+      `.view-country-key-figures .view-content .views-row:nth-child(${i + 1}) p`
+    ).html();
+    let newStr = str.replace(
+      countryNumbers[i],
+      `<span class='counter' data-count=${countryNumbers[i]}>${countryNumbers[i]}</span>`
+    );
+    $(
+      `.view-country-key-figures .view-content .views-row:nth-child(${i + 1}) p`
+    ).html(newStr);
+  }
+
+  function runCounter() {
+    $(".count").each(function () {
+      const This = $(this);
+      This.text(0);
+      $({ Count: This.text() }).animate(
+        { Count: This.attr("data-count") },
+        {
+          duration: 2000,
+          easing: "linear",
+          step: function () {
+            This.text(Math.floor(this.Count));
+          },
+          complete: function () {
+            This.text(this.Count);
+          },
+        }
+      );
+    });
+  }
+
+  const counters = document.querySelectorAll(".counter");
+  const options = { root: null, threshold: 0.5, rootMargin: "-50px" };
+
+  const observer = new IntersectionObserver(function (entries, observer) {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) return;
+
+      entry.target.classList.add("count");
+      runCounter();
+      observer.unobserve(entry.target);
+    });
+  }, options);
+
+  counters.forEach((counter) => {
+    observer.observe(counter);
+  });
+
 })(jQuery, Drupal);
